e32d3940b143c32eabf92381ab273f757312ff11
alexgiul <34978052+alexgiul@users.noreply.github.com>
2024-01-20 19:26:16 +0100

    remove login with basic user & password

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -8,12 +8,6 @@ export default function Register() {
 
   const navigate = useNavigate();
 
-  const adminEmail = 'admin@example.com';
-  const adminPassword = 'qwerty';
-
-  const [email, setEmail] = useState('admin@example.com');
-  const [password, setPassword] = useState('qwerty');
-
   const googleLogin = () => {
 
     localStorage.removeItem("userInfo");
@@ -23,49 +17,6 @@ export default function Register() {
     var login_url = process.env.REACT_APP_BACKEND + '/signup-redirect?auth_provider=' + auth_provider
     window.location.href = login_url
   }
-
-  const handleLogin = e => {
-    e.preventDefault();
-    console.log(e);
-    console.log(email + " - " + password);
-    if (email === adminEmail && password === adminPassword) {
-      Swal.fire({
-        timer: 1500,
-        showConfirmButton: false,
-        willOpen: () => {
-          Swal.showLoading();
-        },
-        willClose: () => {
-          localStorage.setItem('userInfo', JSON.stringify({ "sub": "116655830157832967236", "name": "AChuck Norris", "given_name": "Chuck", "family_name": "Norris", "picture": "https://lh3.googleusercontent.com/a/ACg8ocKr2_xSChXoJh707_7Io8KG9pU-54Lyd0PRfvx25nMALIg=s96-c", "email": "chuck.norris@gmail.com", "email_verified": true, "locale": "en" }));
-
-
-          Swal.fire({
-            icon: 'success',
-            title: 'Successfully logged in!',
-            showConfirmButton: false,
-            timer: 1500,
-          });
-          navigate("/admin")
-        },
-      });
-    } else {
-      Swal.fire({
-        timer: 1500,
-        showConfirmButton: false,
-        willOpen: () => {
-          Swal.showLoading();
-        },
-        willClose: () => {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error!',
-            text: 'Incorrect email or password.',
-            showConfirmButton: true,
-          });
-        },
-      });
-    }
-  };
 
 
   // useEffect(() => {
@@ -97,7 +48,6 @@ export default function Register() {
   return (
     <div className="mt-16 mb-16 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-center">
 
-
       {/* Sign in section */}
       <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
         <h4 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white">
@@ -111,42 +61,6 @@ export default function Register() {
           </button>
         </div>
 
-        <div className="mb-6 mt-6 flex items-center  gap-3">
-          <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
-          <p className="text-base text-gray-600 dark:text-white"> or </p>
-          <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
-        </div>
-
-        <form onSubmit={(e) => handleLogin(e)}>
-          {/* Email */}
-          <InputField
-            variant="auth"
-            extra="mb-3"
-            label="Email*"
-            placeholder="mail@simmmple.com"
-            id="email"
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-
-          {/* Password */}
-          <InputField
-            variant="auth"
-            extra="mb-3"
-            label="Password*"
-            placeholder="Min. 8 characters"
-            id="password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          
-          <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
-            Sign Up
-          </button>
-
-        </form>
       </div>
     </div>
   );
--- src/views/auth/SignIn.jsx
+++ src/views/auth/SignIn.jsx
@@ -144,54 +144,13 @@ export default function SignIn() {
           <span>Sign in with Google</span>
         </button>
 
-        <div className="mb-6 flex items-center  gap-3">
+        <div className="mt-6 mb-6 flex items-center gap-3">
           <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
           <p className="text-base text-gray-600 dark:text-white"> or </p>
           <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
         </div>
 
-        <form onSubmit={(e) => handleLogin(e)}>
-          {/* Email */}
-          <InputField
-            variant="auth"
-            extra="mb-3"
-            label="Email*"
-            placeholder="mail@simmmple.com"
-            id="email"
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
 
-          {/* Password */}
-          <InputField
-            variant="auth"
-            extra="mb-3"
-            label="Password*"
-            placeholder="Min. 8 characters"
-            id="password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          {/* Checkbox */}
-          <div className="mb-4 flex items-center justify-between px-2">
-            <div className="flex items-center">
-              <Checkbox />
-              <p className="ml-2 text-sm font-medium text-navy-700 dark:text-white">
-                Keep me logged In
-              </p>
-            </div>
-            <a
-              className="text-sm font-medium text-brand-500 hover:text-brand-600 dark:text-white"
-              href=" "
-            >
-              Forgot Password?
-            </a>
-          </div>
-          <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
-            Sign In
-          </button>
           <div className="mt-4">
             <span className=" text-sm font-medium text-navy-700 dark:text-gray-600">
               Not registered yet?
@@ -203,7 +162,7 @@ export default function SignIn() {
               Create an account
             </a>
           </div>
-        </form>
+
       </div>
     </div>
   );
